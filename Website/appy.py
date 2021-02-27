23f215b13650201236179e8c5e3bb9791bf0f3dc
ZachKopec <zachkopec@optonline.net>
2021-02-26 20:50:35 -0500

    Route for homepage established

--- Website/appy.py
+++ Website/appy.py
@@ -7,5 +7,11 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
+#Provide a route to the page that is presented when app is initially run 
+@app.route("/")
+def home():
+    #GoTo webpage
+    return render_template("Homepage.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
