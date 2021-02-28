9e04e61bf571523b6d8134746e8fbef2d9139185
ZachKopec <zachkopec@optonline.net>
2021-02-28 18:46:02 -0500

    Updated html and js variable references to flow through form submissions

--- Website/app.py
+++ Website/app.py
@@ -1,9 +1,10 @@
 #Import all required extensions for app to run
 import json
 from flask_pymongo import PyMongo
-from flask import Flask, jsonify, render_template, redirect
+from flask import Flask, jsonify, render_template, request, redirect
 from bson.json_util import dumps
 from pymongo import MongoClient
+from joblib import load
 
 app = Flask(__name__)
 
@@ -27,13 +28,13 @@ def results_page():
 
 #Provide a route that will outsorce our mongodb data as an API to our webpages
 #NOT CREATED YET
-# @app.route("/our-data-api")
-# def scrape():
-#     #Set variable to hold what is returned from calling the function
-#     data = get_data()
-#     #Jsonify the query 
-#     return jsonify(get_data)
-
+@app.route("/our-data-api")
+def scrape():
+    #Set variable to hold what is returned from calling the function
+    pipeline = load("Resources/random_forest.joblib")
+    #Jsonify the query 
+    return pipeline
+    
 # #function that queries database and returns the data
 # def get_data_from_db():
 #     client = MongoClient('mongodb://localhost:27017/')
--- Website/static/js/test.js
+++ Website/static/js/test.js
@@ -5,7 +5,9 @@ const urlParams = new URLSearchParams(queryString);
 
 const gender = urlParams.get('Gender');
 const age = urlParams.get("Age");
-const caffine = urlParams.get("Caffine");
+const height = urlParams.get("Height");
+const weight = urlParams.get("Weight");
+const caffeine = urlParams.get("Caffeine (MG)");
 const systolic = urlParams.get("Systolic");
 const cholesterol = urlParams.get("Cholesterol");
 const drinks = urlParams.get("Drinks");
@@ -14,7 +16,9 @@ const smoker = urlParams.get("Smoker");
 function loaded() {
     document.getElementById("genderInput").innerHTML = gender;
     document.getElementById("ageInput").innerHTML = age;
-    document.getElementById("caffineInput").innerHTML = caffine;
+    document.getElementById("heightInput").innerHTML = height;
+    document.getElementById("weightInput").innerHTML = weight;
+    document.getElementById("caffeineInput").innerHTML = caffeine;
     document.getElementById("systolicInput").innerHTML = systolic;
     document.getElementById("cholesterolInput").innerHTML = cholesterol;
     document.getElementById("drinksInput").innerHTML = drinks;
@@ -23,8 +27,15 @@ function loaded() {
 
 console.log(gender);
 console.log(age);
-console.log(caffine);
+console.log(height);
+console.log(weight);
+console.log(caffeine);
 console.log(systolic);
 console.log(cholesterol);
 console.log(drinks);
 console.log(smoker);
+
+d3.json("/our-data-api", function (mydata) { 
+    data = mydata
+    console.log(data); 
+});
