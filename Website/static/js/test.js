5c54669300d0383a9bcdd42b9b0b20c752d3bd4e
ZachKopec <zachkopec@optonline.net>
2021-03-03 20:33:35 -0500

    Edited results

--- Website/static/js/test.js
+++ Website/static/js/test.js
@@ -13,19 +13,37 @@ const cholesterol = urlParams.get("Cholesterol");
 const drinks = urlParams.get("Drinks");
 const smoker = urlParams.get("Smoker");
 
-function loaded() {
-    document.getElementById("genderInput").innerHTML = gender;
-    document.getElementById("ageInput").innerHTML = age;
-    document.getElementById("heightInput").innerHTML = height;
-    document.getElementById("weightInput").innerHTML = weight;
-    document.getElementById("caffeineInput").innerHTML = caffeine;
-    document.getElementById("systolicInput").innerHTML = systolic;
-    document.getElementById("cholesterolInput").innerHTML = cholesterol;
-    document.getElementById("drinksInput").innerHTML = drinks;
-    document.getElementById("smokerInput").innerHTML = smoker;
+if (gender == "0") {
+    var genderText = "Male" 
+} else {
+    var genderText = "Female"
 }
 
-console.log(gender);
+if (smoker == "0") {
+    var smokerText = "Yes" 
+} else {
+    var smokerText = "No"
+}
+
+if (age == "1") {
+    var ageS = " year old" 
+} else {
+    var ageS = " years old"
+}
+
+function loaded() {
+    document.getElementById("genderInput").innerHTML = genderText;
+    document.getElementById("ageInput").innerHTML = age + ageS
+    document.getElementById("heightInput").innerHTML = height + " CM";
+    document.getElementById("weightInput").innerHTML = weight + " KG";
+    document.getElementById("caffeineInput").innerHTML = caffeine + " MG";
+    document.getElementById("systolicInput").innerHTML = systolic + " mm Hg";
+    document.getElementById("cholesterolInput").innerHTML = cholesterol + " mg/dL";
+    document.getElementById("drinksInput").innerHTML = drinks;
+    document.getElementById("smokerInput").innerHTML = smokerText;
+}
+
+console.log(genderText);
 console.log(age);
 console.log(height);
 console.log(weight);
@@ -35,6 +53,6 @@ console.log(cholesterol);
 console.log(drinks);
 console.log(smoker);
 
-params = [gender, age, height, weight, caffeine, systolic, cholesterol, drinks, smoker]
+params = [genderText, age, height, weight, caffeine, systolic, cholesterol, drinks, smoker]
 
 console.log(params)
